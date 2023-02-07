aa338f3ef0fd3bfdd2e77899a44bd9ebcd9bde4c
Collapsare <alexandr0899@mail.ru>
2023-02-07 11:54:57 +0300

    added div for numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const btnStart = document.getElementById('btn-start')
 const btnEnd = document.getElementById('btn-end')
 const audio = document.getElementById('audio')
+const number = document.getElementById('number')
 
 btnStart.addEventListener('click', start)
 btnEnd.addEventListener('click', end)
@@ -8,8 +9,10 @@ btnEnd.addEventListener('click', end)
 
 function end() {
     audio.pause()
+
 }
+
 function start() {
     audio.play()
-    console.log('hi');
+    
 }
