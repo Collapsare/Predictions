47f8a8a4dc6629096d7e09e26bce5cb02667abe2
Collapsare <alexandr0899@mail.ru>
2023-02-07 12:33:23 +0300

    added timer animation

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const btnEnd = document.getElementById('btn-end')
 const audio = document.getElementById('audio')
 const number = document.getElementById('number')
 const numberWrapper = document.getElementById('number-wrapper')
-
+let flag = true
 const predictions = [1, 2, 3, 4, 5]
 
 
@@ -14,15 +14,27 @@ function getPredictionNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function printNum() {
+    setTimeout(function go() {
+        number.innerText = predictions[getPredictionNumber(0, 5)]
+        if (flag) {
+            setTimeout(go, 100)
+        }
+    }, 100)
+}
+
 function start() {
     audio.play()
     numberWrapper.classList.add('appear')
-    number.innerText = predictions[getPredictionNumber(0, 5)]
+    printNum()
+    
+    
 }
 
 function end() {
     audio.pause()
-    numberWrapper.classList.remove('appear')
-
+    flag = false
+    btnStart.classList.add('disappear')
+    btnEnd.classList.add('disappear')
 }
 
